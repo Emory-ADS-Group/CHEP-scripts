1eaa0492754b4399ea5c1adb44db5f6c85c355d5
Mo Zhang <mo.zhang@emory.edu>
2016-02-24 21:36:30 -0500

    findMatch.r bug fix
    Fixes error “Error in distReturn[, 2] : subscript out of bounds”

--- findMatch.R
+++ findMatch.R
@@ -1,27 +1,39 @@
 findMatch <- function(locationUniq, locationDB, matchCrit) {
-  
-  
+
   a <- !is.na(match(trunc(locationDB[,1], prec = 0), trunc(locationUniq[1], prec = 0)))
   b <- !is.na(match(trunc(locationDB[,2], prec = 0), trunc(locationUniq[2], prec = 0)))
   c <- ((a == TRUE) & (b == TRUE))
   
-  rowIndex <- which(c)
+  if (sum(c) > 0) {
+    
+    
+    rowIndex <- which(c)
+    
+    combodata <- rbind(locationDB[rowIndex,], locationUniq)
+    
+    distMatrix <- distm(cbind(combodata[2], combodata[1]), fun =distHaversine)
+    
+    n <- nrow(combodata)
+    
+    distReturn <- cbind(as.integer(rowIndex), distMatrix[n,1:(n-1)])  
+    
+    matchDist <- which((distReturn[,2] < matchCrit))
+    
+    matchDF <- cbind(rowIndex[matchDist], distReturn[matchDist,2])
+    
+    #  matchDF <- distReturn[which(matchDist),]
+    
+    return(matchDF)
+    
+    
+  } else {
+    
+    matchDF <- NA
+    return(matchDF)
+    
+  }
   
-  combodata <- rbind(locationDB[rowIndex,], locationUniq)
   
-  distMatrix <- distm(cbind(combodata[2], combodata[1]), fun =distHaversine)
-  
-  n <- nrow(combodata)
-   
-  distReturn <- cbind(as.integer(rowIndex), distMatrix[n,1:(n-1)])  
-  
-  matchDist <- which((distReturn[,2] < matchCrit))
-  
-  matchDF <- cbind(rowIndex[matchDist], distReturn[matchDist,2])
-  
-#  matchDF <- distReturn[which(matchDist),]
-  
-  return(matchDF)
   
 }
 

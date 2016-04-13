dcaca964bbfb29aee6142ee4eec007b3865dfeaf
mandarpra <mandarpra@gmail.com>
2016-04-13 09:28:46 -0400

    Latest Trips.R file
    Removed the check for ping type

--- trips.R
+++ trips.R
@@ -24,8 +24,8 @@ combined_assets<- ReducedEvents[whCircle,]
 #get only the pings between clusters and with type 3 and 4
 cluster1_Assets <- ReducedEvents[whCircle1,]
 cluster2_Assets <- ReducedEvents[whCircle2,]
-cluster1_Assets <- cluster1_Assets[cluster1_Assets$type == 3 |cluster1_Assets$type == 4,]
-cluster2_Assets <- cluster2_Assets[cluster2_Assets$type == 3 |cluster2_Assets$type == 4,]
+#cluster1_Assets <- cluster1_Assets[cluster1_Assets$type == 3 |cluster1_Assets$type == 4,]
+#cluster2_Assets <- cluster2_Assets[cluster2_Assets$type == 3 |cluster2_Assets$type == 4,]
 
 #order pings according to date time from oldest to latest
 cluster1_Assets<- cluster1_Assets[order(cluster1_Assets$datetime),]
@@ -35,7 +35,7 @@ cluster2_Assets$cluster<-2
 
 #Fetch assets that have been to both sites
 common_assets <- unique((semi_join(cluster1_Assets,cluster2_Assets,by = "assetID"))$assetID)
-common_id <- unique((semi_join(cluster1_Assets,cluster2_Assets,by = "assetID"))$id)
+#common_id <- unique((semi_join(cluster1_Assets,cluster2_Assets,by = "assetID"))$id)
 
 #combine the pings
 combined_pings<- rbind(cluster1_Assets,cluster2_Assets)
@@ -44,6 +44,7 @@ combined_pings<- rbind(cluster1_Assets,cluster2_Assets)
 trips = data.frame(trip_id=NA,start_id = NA, end_id = NA,assetID=NA, start_lat=NA,start_lon=NA,end_lat=NA,end_lon=NA,start_time="",end_time="")
 trips$start_time = as.character(trips$start_time)
 trips$end_time = as.character(trips$end_time)
+trips$distance = NA
 trip_num=1
 #finding trips between two  clusters
 for (i in common_assets){
@@ -58,7 +59,8 @@ for (j in 1:nrow(cluster_assets)){
     }
   else{
     #identify last move event from one location and first stop event at next location
-    if(cluster_assets[j,]$cluster!=prev_loc && cluster_assets[j-1,]$type == 3){
+    if(cluster_assets[j,]$cluster!=prev_loc){
+      trips[trip_num,]$distance <- HVDist(c(cluster_assets[j-1,]$latitude,cluster_assets[j-1,]$longitude),c(cluster_assets[j,]$latitude,cluster_assets[j-1,]$longitude))
       trips[trip_num,]$trip_id <- paste(c(cluster_assets[j-1,]$id,"00",trip_num),collapse = "") 
       trips[trip_num,]$start_id <- cluster_assets[j-1,]$id
       trips[trip_num,]$end_id <- cluster_assets[j,]$id
